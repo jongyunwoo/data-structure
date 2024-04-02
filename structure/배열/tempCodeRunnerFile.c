714c1abfc7ca2c80e80f6052767f73fd7dd68524
jongyunwoo <woojy4598@gmail.com>
2024-04-02 13:26:44 +0900

    add : code

--- structure/배열/tempCodeRunnerFile.c
+++ structure/배열/tempCodeRunnerFile.c
@@ -3,71 +3,46 @@
 #include <string.h>
 #pragma warning(disable:4996)
 
-int main()
+
+int main() 
 {
-   int N, M;
-   int a = 1;
-   int i = 0, j = 0;
-   scanf("%d %d", &N, &M);
-   int **arr = (int **)malloc(N * sizeof(int *));
-   for (int i = 0; i < N; i++){
-      arr[i] = (int *)malloc(M * sizeof(int));
-   }
-   int countN = N;
-   int countM = M;
-   int countm = 0;
-   int countn = 0;
-   while(a <= N*M){
-      for(;;){
-         arr[j][i] = a;
-         i++;
-         a++;
-         if(i == countM){
-            i--;
-            j++;
-            countM--;
-            break;
-         }
-      }
-      for(;;){
-         arr[j][i] = a;
-         a++;
-         j++;
-         if(j == countN){
-            j--;
-            i--;
-            countN--;
-            break;
-         }
-      }
-      for(;;){
-         arr[j][i] = a;
-         i--;
-         a++;
-         if(i < countm){
-            i++;
-            j--;
-            countm++;
-            break;
-         }
-      }
-      for(;;){
-         arr[j][i] = a;
-         a++;
-         j--;
-         if(j == countn){
-            i++;
-            j++;
-            countn++;
-            break;
-         }
-      }
-   }
-   for(int k = 0; k < N; k++){
-      for(int f = 0; f < M; f++){
-         printf(" %d", arr[k][f]);
-      }
-      printf("\n");
-   }
-   return 0;
+    int N, M;
+    int row, col = 0;
+    scanf("%d %d", &N, &M);
+    int **arr =(int **)malloc(N*sizeof(int*));
+    for(int i = 0; i < N; i++){
+        arr[i] = (int *)malloc(M*sizeof(int));
+    }
+    int a = 1; 
+
+    // 대각선 방향으로 숫자 채우기,
+    for(int d = 0; d < N + M - 1; d++) {
+        if(d < M){
+            row = 0;
+            col = d;
+        }
+        else if(d >= M){
+            row = d - M + 1;
+            col = M - 1;
+        }
+        while(row < N && col >= 0) {
+            arr[row][col] = a++;
+            row++;
+            col--;
+        }
+    }
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < M; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    for(int i = 0; i < N; i++){
+        free(arr[i]);
+    }
+    free(arr);
+
+    return 0;
 }
+
