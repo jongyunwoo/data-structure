eea8d270f5600d100601b92d1bdc0d2febf05236
jongyunwoo <woojy4598@gmail.com>
2024-04-03 11:58:19 +0900

    add : code

---
+++ structure/배열/연결리스트/1.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    char e;
+    struct Node* prev;
+    struct Node* next;
+} Node;
+
+Node* getNode() {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void putNode(Node* p) {
+    free(p);
+}
+
+Node* init(Node** H, Node** T) {
+    *H = getNode();
+    *T = getNode();
+    (*H)->next = *T;
+    (*T)->prev = *H;
+    return *H;
+}
+
+void print(Node* H, Node* T) {
+    Node* p = H->next;
+    while (p != T) {
+        printf("%c", p->e);
+        p = p->next;
+    }
+    printf("\n");
+}
+
+void addNodeBefore(Node* p, char e) {
+    Node* newNode = getNode();
+    newNode->e = e;
+    newNode->prev = p->prev;
+    newNode->next = p;
+    p->prev->next = newNode;
+    p->prev = newNode;
+}
+
+void add(Node* H, Node* T, int r, char e, int* n) {
+    if (r < 1 || r > (*n) + 1) {
+        printf("invalid rank exception\n");
+        return;
+    }
+    Node* p = H;
+    for (int i = 0; i < r; i++) {
+        p = p->next;
+    }
+    addNodeBefore(p, e);
+    (*n)++;
+}
+
+char removeNode(Node* p) {
+    char e = p->e;
+    p->prev->next = p->next;
+    p->next->prev = p->prev;
+    putNode(p);
+    return e;
+}
+
+char delete(Node* H, Node* T, int r, int* n) {
+    if (r < 1 || r > *n) {
+        printf("invalidRankException\n");
+        return '\0';
+    }
+    Node* p = H;
+    for (int i = 0; i < r; i++) {
+        p = p->next;
+    }
+    char e = removeNode(p);
+    (*n)--;
+    return e;
+}
+
+char get(Node* H, Node* T, int r) {
+    if (r < 1) {
+        printf("invalid position\n");
+        return '\0';
+    }
+    Node* current = H;
+    for (int i = 0; i < r; i++) {
+        if (current->next == T) {
+            printf("invalid position\n");
+            return '\0';
+        }
+        current = current->next;
+    }
+    return current->e;
+}
+
+int main() {
+    Node* H = NULL;
+    Node* T = NULL;
+    H = init(&H, &T);
+    int n = 0; // 연산의 개수
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        char operation;
+        int r;
+        char e;
+        scanf(" %c %d", &operation, &r);
+        if (operation == 'A') {
+            scanf(" %c", &e);
+            add(H, T, r, e, &n);
+        } else if (operation == 'D') {
+            delete(H, T, r, &n);
+        } else if (operation == 'G') {
+            printf("%c\n", get(H, T, r));
+        } else if (operation == 'P') {
+            print(H, T);
+        }
+    }
+
+    return 0;
+}
